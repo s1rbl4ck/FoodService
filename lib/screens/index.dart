dd0d35894cade64017f4e5eb845593b41ef7790e
= <jack.82janson@gmail.com>
2022-02-19 00:52:41 +0330

    Transfered SingleChildScrollView from index screen to home & search screen

---
+++ lib/screens/index.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:foodservice/controllers/bottomNavigationController.dart';
+import 'package:foodservice/controllers/homeController.dart';
+import 'package:foodservice/widgets/home/appBar.dart';
+import 'package:foodservice/widgets/map/indexScreenWidget.dart';
+import 'package:foodservice/widgets/search/appBar.dart';
+import 'package:foodservice/widgets/search/indexScreen.dart';
+import 'package:get/get.dart';
+import 'package:foodservice/widgets/shimmer/mainFoodWidget.Dart';
+import 'package:foodservice/widgets/shimmer/mainBannerWidget.dart';
+import 'package:foodservice/widgets/shimmer/mainProfileWidget.dart';
+import 'package:foodservice/widgets/drawerWidget.dart';
+import 'package:foodservice/widgets/bottomNavigatorWidget.dart';
+import 'package:foodservice/widgets/home/indexScreen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void replaceShimmer() {
+    Get.find<HomeController>().isDataRecieved.value =
+        !Get.find<HomeController>().isDataRecieved.value;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), () {
+      replaceShimmer();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget routeContent() {
+      if (Get.find<HomeController>().isDataRecieved.value) {
+        switch (Get.find<BNavigationController>().selectedIndex.value) {
+          case 0:
+            return HomeScreenWidget();
+          case 1:
+            return SearchScreenWidget();
+          case 2:
+            return Text('Basket Screen');
+          case 3:
+            return Text('Receipt Screen');
+          case 4:
+            return MapScreenWidget();
+          default:
+            return Text('No Where');
+        }
+      } else {
+        return Column(
+          children: [
+            ShimmerBannerWidget(),
+            ShimmerProfilesWidget(),
+            ShimmerFoodWidget(),
+            ShimmerFoodWidget(),
+            ShimmerFoodWidget(),
+            ShimmerFoodWidget(),
+          ],
+        );
+      }
+    }
+
+    AppBar appBarRoute() {
+      switch (Get.find<BNavigationController>().selectedIndex.value) {
+        case 0:
+          return HomeAppBarWidget();
+        case 1:
+          return SearchAppBarWidget();
+        case 2:
+          return HomeAppBarWidget();
+        case 3:
+          return HomeAppBarWidget();
+        case 4:
+          return HomeAppBarWidget();
+        default:
+          return HomeAppBarWidget();
+      }
+    }
+
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Obx(
+        () => Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: appBarRoute(),
+          drawer: CustomDrawer(),
+          bottomNavigationBar: BottomNavigator(),
+          body: routeContent(),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/home/indexScreen.dart
+++ lib/widgets/home/indexScreen.dart
@@ -20,179 +20,181 @@ class HomeScreenWidget extends StatefulWidget {
 class _HomeScreenWidgetState extends State<HomeScreenWidget> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Home Banners
-        Container(
-          width: Get.width,
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Container(
-            height: 200,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // Home Banners
+          Container(
+            width: Get.width,
+            margin: EdgeInsets.all(10),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(8),
             ),
-            child: Column(
-              children: [
-                Expanded(
-                  child: CarouselSlider(
-                    items: imageSliders,
-                    carouselController:
-                        Get.find<HomeController>().sliderController,
-                    options: CarouselOptions(
-                      autoPlay: true,
-                      enlargeCenterPage: true,
-                      viewportFraction: 1,
-                      aspectRatio: 2.0,
-                      onPageChanged: (index, reason) {
-                        Get.find<HomeController>().setCurrentSlide(index);
-                      },
+            child: Container(
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: CarouselSlider(
+                      items: imageSliders,
+                      carouselController:
+                          Get.find<HomeController>().sliderController,
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        enlargeCenterPage: true,
+                        viewportFraction: 1,
+                        aspectRatio: 2.0,
+                        onPageChanged: (index, reason) {
+                          Get.find<HomeController>().setCurrentSlide(index);
+                        },
+                      ),
                     ),
                   ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: Get.width,
+            height: 90,
+            padding: EdgeInsets.all(5),
+            margin: EdgeInsets.fromLTRB(8, 2, 8, 2),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                CategoriesWidget(
+                  imageUrl: 'assets/images/hamburger.png',
+                  name: 'فست فود',
+                ),
+                CategoriesWidget(
+                  imageUrl: 'assets/images/restaurant.png',
+                  name: 'رستوران',
+                ),
+                CategoriesWidget(
+                  imageUrl: 'assets/images/traditional.png',
+                  name: 'غذای خانگی',
+                ),
+                CategoriesWidget(
+                  imageUrl: 'assets/images/cafe.png',
+                  name: 'کافه',
+                ),
+                CategoriesWidget(
+                  imageUrl: 'assets/images/fruits.png',
+                  name: 'میوه',
+                ),
+                CategoriesWidget(
+                  imageUrl: 'assets/images/shops.png',
+                  name: 'سوپرمارکت',
+                ),
+                CategoriesWidget(
+                  imageUrl: 'assets/images/cake.png',
+                  name: 'شیرینی',
                 ),
               ],
             ),
           ),
-        ),
-        Container(
-          width: Get.width,
-          height: 90,
-          padding: EdgeInsets.all(5),
-          margin: EdgeInsets.fromLTRB(8, 2, 8, 2),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'پرسفارش ترین ها',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Wrap(
+                  children: [
+                    Text('مشاهده بیشتر'),
+                    Icon(Icons.chevron_right_outlined)
+                  ],
+                ),
+              ],
+            ),
           ),
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              CategoriesWidget(
-                imageUrl: 'assets/images/hamburger.png',
-                name: 'فست فود',
-              ),
-              CategoriesWidget(
-                imageUrl: 'assets/images/restaurant.png',
-                name: 'رستوران',
-              ),
-              CategoriesWidget(
-                imageUrl: 'assets/images/traditional.png',
-                name: 'غذای خانگی',
-              ),
-              CategoriesWidget(
-                imageUrl: 'assets/images/cafe.png',
-                name: 'کافه',
-              ),
-              CategoriesWidget(
-                imageUrl: 'assets/images/fruits.png',
-                name: 'میوه',
-              ),
-              CategoriesWidget(
-                imageUrl: 'assets/images/shops.png',
-                name: 'سوپرمارکت',
-              ),
-              CategoriesWidget(
-                imageUrl: 'assets/images/cake.png',
-                name: 'شیرینی',
-              ),
-            ],
+          Container(
+            height: 320,
+            margin: EdgeInsets.all(10),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                RestaurantItem(
+                  title: 'رستوران کاپوچینو',
+                  category: 'رستوران',
+                  description:
+                      'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است',
+                  coverImageUrl: 'assets/images/restaurant_cover.jpg',
+                  imageUrl: 'assets/images/default_profiles.png',
+                  isOpened: true,
+                  deliveryPrice: '10,000 تومان',
+                  distance: '1 کیلومتر',
+                ),
+                RestaurantItem(
+                  title: 'فست فودی رومانو',
+                  category: 'فست فود',
+                  description:
+                      'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است',
+                  coverImageUrl: 'assets/images/fastfood_cover.jpg',
+                  imageUrl: 'assets/images/default_profiles.png',
+                  isOpened: false,
+                  deliveryPrice: 'رایگان',
+                  distance: '600 متر',
+                )
+              ],
+            ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'پرسفارش ترین ها',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Wrap(
-                children: [
-                  Text('مشاهده بیشتر'),
-                  Icon(Icons.chevron_right_outlined)
-                ],
-              ),
-            ],
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'دسته بندی فست فود',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Wrap(
+                  children: [
+                    Text('مشاهده بیشتر'),
+                    Icon(Icons.chevron_right_outlined)
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-        Container(
-          height: 320,
-          margin: EdgeInsets.all(10),
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              RestaurantItem(
-                title: 'رستوران کاپوچینو',
-                category: 'رستوران',
-                description:
-                    'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است',
-                coverImageUrl: 'assets/images/restaurant_cover.jpg',
-                imageUrl: 'assets/images/default_profiles.png',
-                isOpened: true,
-                deliveryPrice: '10,000 تومان',
-                distance: '1 کیلومتر',
-              ),
-              RestaurantItem(
-                title: 'فست فودی رومانو',
-                category: 'فست فود',
-                description:
-                    'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است',
-                coverImageUrl: 'assets/images/fastfood_cover.jpg',
-                imageUrl: 'assets/images/default_profiles.png',
-                isOpened: false,
-                deliveryPrice: 'رایگان',
-                distance: '600 متر',
-              )
-            ],
+          Container(
+            height: 285,
+            margin: EdgeInsets.all(10),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                FoodItem(
+                  name: 'پیتزا پپرونی',
+                  imageUrl: 'assets/images/pizzaProduct.png',
+                  price: '56,000',
+                  category: 'فست فود',
+                ),
+                FoodItem(
+                  name: 'همبرگر زغالی',
+                  imageUrl: 'assets/images/burger.png',
+                  price: '42,500',
+                  category: 'فست فود',
+                ),
+                FoodItem(
+                  name: 'ژامبون کالباس و مرغ',
+                  imageUrl: 'assets/images/sandwitch.png',
+                  price: '25,000',
+                  category: 'فست فود',
+                ),
+              ],
+            ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'دسته بندی فست فود',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Wrap(
-                children: [
-                  Text('مشاهده بیشتر'),
-                  Icon(Icons.chevron_right_outlined)
-                ],
-              ),
-            ],
-          ),
-        ),
-        Container(
-          height: 285,
-          margin: EdgeInsets.all(10),
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              FoodItem(
-                name: 'پیتزا پپرونی',
-                imageUrl: 'assets/images/pizzaProduct.png',
-                price: '56,000',
-                category: 'فست فود',
-              ),
-              FoodItem(
-                name: 'همبرگر زغالی',
-                imageUrl: 'assets/images/burger.png',
-                price: '42,500',
-                category: 'فست فود',
-              ),
-              FoodItem(
-                name: 'ژامبون کالباس و مرغ',
-                imageUrl: 'assets/images/sandwitch.png',
-                price: '25,000',
-                category: 'فست فود',
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
